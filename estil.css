07c9cf20b953bc5e15f511ec7926484e4c34cb8b
Mmoya123 <82730331+Mmoya123@users.noreply.github.com>
2023-03-01 16:03:06 -0300

    Create estil.css

---
+++ estil.css
@@ -0,0 +1,14 @@
+
+document.getElementById("boton").onclick = function() {
+
+    console.log("capturaamos el evento click");
+    document.getElementById("demo").innerHTML = "Hola, mi nombre es: Estefanía Maderas";
+}
+
+document.getElementById("boton_ex").onclick = function() {
+    document.getElementById("demo").innerHTML = "Secretaria de coordinación y administración";
+}
+
+document.getElementById("boton_pr").onclick = function() {
+    document.getElementById("demo").innerHTML = "Proactividad, Trabajo en equipo y Habilidades blandas";
+}
